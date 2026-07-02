d3bf4a95f3ebb6dbd61cad00ed7ea7d26d81f45f
wys0530 <youn.soo.woo@gmail.com>
2026-07-03 02:27:33 +0900

    init: 프로젝트 초기 설정

---
+++ src/pages/Home/Home.style.js
@@ -0,0 +1,14 @@
+//style 파일 예시
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: var(--main); //index.css의 :root 내부에 자주 쓰는 컬러 추가 후 사용
+`;
+
+export const LockIcon = styled.img`
+  width: 5rem;
+  height: 5rem;
+  background-color: var(--black);
+`;
