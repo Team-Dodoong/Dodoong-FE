c9cfa05dc6da674e0140b326bf2c9dc083f75f88
gusqkr <hyeon05@ewha.ac.kr>
2026-07-24 19:24:11 +0900

    feat: 파티 개설 페이지 UI 구현

--- src/router.jsx
+++ src/router.jsx
@@ -7,25 +7,26 @@ import Home from "./pages/Home/Home.jsx";
 import Quest from "./pages/Quest/Quest/Quest.jsx";
 import Character from "./pages/Character/Character/Character.jsx";
 import Party from "./pages/Party/Party/Party.jsx";
+import PartyCreate from "./pages/Party/PartyCreate/PartyCreate.jsx";
 import MyPage from "./pages/MyPage/MyPage.jsx";
 
 // 공통 레이아웃 컴포넌트
 const Layout = () => {
-
   const location = useLocation(); // 현재 브라우저의 주소 경로를 가져옴
 
   // 현재 경로가 '/' (로그인 화면)이 아닐 때만 BottomNav를 노출하도록 설정
   const showBottomNav =
-  location.pathname !== "/" &&
-  location.pathname !== "/onboarding";
+    location.pathname !== "/" && location.pathname !== "/onboarding";
 
   return (
-    <div style={{ 
-      position: 'relative',    // 하단 바가 이 안에서만 절대 위치(absolute)를 잡도록 기준점이 되어줌.
-      maxWidth: '480px',       // index.css에서 지정한 최대 넓이
-      margin: '0 auto',        // 화면 중앙 정렬
-      minHeight: '100vh',      // 화면 세로 꽉 채우기
-    }}>
+    <div
+      style={{
+        position: "relative", // 하단 바가 이 안에서만 절대 위치(absolute)를 잡도록 기준점이 되어줌.
+        maxWidth: "480px", // index.css에서 지정한 최대 넓이
+        margin: "0 auto", // 화면 중앙 정렬
+        minHeight: "100vh", // 화면 세로 꽉 채우기
+      }}
+    >
       {/* Outlet - 현재 주소에 맞는 하위 페이지(Home, Quest 등)가 갈아끼워져 나오는 공간 */}
       <Outlet />
 
@@ -45,6 +46,7 @@ const router = createBrowserRouter([
       { path: "home", element: <Home /> }, // '/home' 주소일 때
       { path: "quest", element: <Quest /> }, // '/quest' 주소일 때
       { path: "party", element: <Party /> }, // '/party' 주소일 때
+      { path: "party/create", element: <PartyCreate /> },
       { path: "character", element: <Character /> },
       { path: "mypage", element: <MyPage /> },
     ],
