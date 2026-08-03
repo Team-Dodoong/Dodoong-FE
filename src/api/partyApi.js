4a920c63771a7f22f4e68d75e03dc2836715c059
gusqkr <hyeon05@ewha.ac.kr>
2026-08-04 03:44:35 +0900

    feat: 파티 검색, 내 파티 조회 api 연결

--- src/api/partyApi.js
+++ src/api/partyApi.js
@@ -1,3 +1,21 @@
 import instance from "./instance";
 
 export const createParty = (data) => instance.post("/api/parties", data);
+
+export const searchParties = ({
+  keyword,
+  categories,
+  page = 0,
+  size = 20,
+} = {}) =>
+  instance.get("/api/parties/search", {
+    params: {
+      keyword,
+      categories: categories?.length ? categories.join(",") : undefined,
+      page,
+      size,
+    },
+  });
+
+export const getMyParties = ({ page = 0, size = 20 } = {}) =>
+  instance.get("/api/parties/my", { params: { page, size } });
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,14 @@
 import * as S from "./SearchBar.style";
 
-function SearchBar({ placeholder }) {
+function SearchBar({ placeholder, value, onChange, onKeyDown }) {
   return (
     <S.SearchWrapper>
-      <S.Input placeholder={placeholder} />
+      <S.Input
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+      />
       <S.SearchIcon />
     </S.SearchWrapper>
   );
