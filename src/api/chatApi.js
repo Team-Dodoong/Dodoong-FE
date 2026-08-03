b9831a7e5faa6d86bb5dd43f8c88aea35a956c09
gusqkr <hyeon05@ewha.ac.kr>
2026-08-04 05:18:09 +0900

    feat: 채팅 내역 조회 api 연결

--- src/api/chatApi.js
+++ src/api/chatApi.js
@@ -1,3 +1,8 @@
 import instance from "./instance";
 
 export const getChatRooms = () => instance.get("/api/chat-rooms");
+
+export const getChatHistory = (partyId, { cursor, size = 30 } = {}) =>
+  instance.get(`/api/parties/${partyId}/chats`, {
+    params: { cursor, size },
+  });
