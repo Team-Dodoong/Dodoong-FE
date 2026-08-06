a61e259f9ea3ea38f0bc1e95bf946726f053f75d
gusqkr <hyeon05@ewha.ac.kr>
2026-08-07 06:38:37 +0900

    design: 상세보기 모달 UI 디자인 디테일 수정

--- src/pages/Character/components/DetailModal.style.js
+++ src/pages/Character/components/DetailModal.style.js
@@ -1,11 +1,17 @@
 import styled from "styled-components";
-import { SemiBold_16, Regular_14, Medium_14 } from "../../../styles/Fonts";
+import {
+  SemiBold_16,
+  Regular_12,
+  Medium_14,
+  SemiBold_14,
+} from "../../../styles/Fonts";
+import Close from "../../../assets/ic_linear_cancel_24.svg?react";
 
 export const Overlay = styled.div`
   position: fixed;
   inset: 0;
   background: rgba(0, 0, 0, 0.4);
-  z-index: 100;
+  z-index: 1001;
 `;
 
 export const Modal = styled.div`
@@ -15,60 +21,67 @@ export const Modal = styled.div`
   transform: translate(-50%, -50%);
   width: 18.4375rem;
   background: white;
-  border-radius: 1rem;
-  padding: 1.75rem 1.25rem 1.25rem;
-  z-index: 101;
+  border-radius: 1.25rem;
+  padding: 56px 1.25rem 1.25rem;
+  z-index: 1002;
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 0.75rem;
+  gap: 1rem;
   max-height: 80vh;
   overflow-y: auto;
 `;
 
-export const CloseButton = styled.button`
+export const CloseButton = styled(Close)`
   position: absolute;
-  top: 1rem;
-  left: 1rem;
+  top: 1.25rem;
+  left: 1.25rem;
   background: none;
   border: none;
-  font-size: 1.25rem;
-  color: var(--Gray_5);
+  font-size: 1.5rem;
+  color: #202023;
   cursor: pointer;
 `;
 
 export const CharacterImage = styled.img`
-  width: 8rem;
-  height: 8rem;
+  width: auto;
+  height: 154px;
   object-fit: contain;
 `;
 
 export const CharacterName = styled.h3`
-  ${SemiBold_16};
+  font-family: "Jalnan2";
+  font-size: 16px;
+  font-weight: 400;
   color: var(--Black);
 `;
 
 export const CharacterSubTitle = styled.p`
-  ${Medium_14};
-  color: var(--Gray_6);
+  ${SemiBold_14};
+  color: var(--Gray_7);
 `;
 
 export const CharacterDescription = styled.p`
-  ${Regular_14};
-  color: var(--Gray_5);
+  ${Regular_12};
+  color: var(--Gray_7);
   text-align: center;
-  line-height: 1.7;
   white-space: pre-line;
+  word-break: keep-all;
+  padding: 0 10px;
 `;
 
 export const BuyButton = styled.button`
   width: 100%;
-  height: 3rem;
-  border-radius: 8px;
+  height: 2.25rem;
+  border-radius: 4px;
   border: none;
   background: var(--Black);
-  color: var(--Main);
-  ${SemiBold_16};
+  color: var(--White);
+  ${SemiBold_14};
   cursor: pointer;
-  margin-top: 0.5rem;
+  margin-top: 0.25rem;
+`;
+
+export const CharacterCost = styled.span`
+  color: var(--Main);
 `;
